fdc603d2cc1fa954d8b9d869507edff9b4319f01
Axxel-otl <Axxel.Inkordia@proton.me>
2026-07-29 14:13:55 -0600

    test!(Some tests. Will be repaired in the next commit.): Umm... Yeah.

--- arch/.config/zsh/startup.zsh
+++ arch/.config/zsh/startup.zsh
@@ -3,10 +3,10 @@
 if [[ -z "$FASTFETCH_SHOWN" ]]; then
   export FASTFETCH_SHOWN=1
 
-  logos=(${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/*(.N))
+  logos=(${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/*.(png|jpg|jpeg)(.N))
 
   if (( ${#logos[@]} )); then
-    fastfetch --logo "${logos[RANDOM % ${#logos[@]} + 1]}"
+    fastfetch --logo "${logos[$((RANDOM % ${#logos[@]} + 1))]}"
   else
     fastfetch
   fi
