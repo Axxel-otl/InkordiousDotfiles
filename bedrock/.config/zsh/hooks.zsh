75928c92fc794dc5cb19716c3923a6c6391f0d8c
Axxel-otl <Axxel.Inkordia@proton.me>
2026-08-23 17:58:24 -0600

    fix(./bedrock/.config/zsh/hooks.zsh): Fix the timer array to be way more specific, to count the time correctly and to not send multiple notifications

--- bedrock/.config/zsh/hooks.zsh
+++ bedrock/.config/zsh/hooks.zsh
@@ -1,19 +1,32 @@
 skip_timer=0
 cmd_start=0
+cmd_running=0
 LAST_EXIT=0
 
 preexec() {
-  cmd_start=$SECONDS
+  cmd_start=$EPOCHREALTIME
+  cmd_running=1
 }
 
 notify_long_command() {
-  local duration=$((SECONDS - cmd_start))
-  (( duration > 10 )) && notify-send "Comando terminado" "Duró ${duration}s"
+  if (( cmd_running )); then
+    local duration=$((EPOCHREALTIME - cmd_start))
+
+    if (( duration > 10 )); then
+      printf -v duration "%.2f" "$duration"
+      notify-send "Comando terminado" "Duró ${duration}s"
+    fi
+
+    cmd_running=0
+  fi
 }
 
 save_last_exit() {
   LAST_EXIT=$?
 }
 
-precmd_functions+=(notify_long_command)
-precmd_functions+=(save_last_exit)
+(( ${precmd_functions[(I)notify_long_command]} )) ||
+  precmd_functions+=(notify_long_command)
+
+(( ${precmd_functions[(I)save_last_exit]} )) ||
+  precmd_functions+=(save_last_exit)
