89db0e792bc40ab0f66b507e82799509b2693741
Axxel-otl <Axxel.Inkordia@proton.me>
2026-08-25 15:09:12 -0600

    style!(./{arch,bedrock}/.config/zsh/aliases.zsh): Rename Zreload function to ZReload to match ZList and ZApply

--- bedrock/.config/zsh/aliases.zsh
+++ bedrock/.config/zsh/aliases.zsh
@@ -1,2 +1,2 @@
 alias Limpiar='clear && fastfetch --logo "$(find "${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/" -name "*.png" | shuf -n 1)"'
-alias Zreload='source ~/.config/zsh/.zshrc'
+alias ZReload='source ~/.config/zsh/.zshrc'
